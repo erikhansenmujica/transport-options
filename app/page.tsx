f6ee374c07e754a13d724b8795880bb87866512d
Erik Hansen <erikhansenmujica@gmail.com>
2023-09-07 17:45:40 +0200

    added number if buttons don't work

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,9 @@ export default function Home() {
               <p className="mb-5 font-light text-gray-500 sm:text-xl ">
                 We offer a variety of transport options to suit your needs.
               </p>
+              <p className="mb-5 font-light text-gray-500 sm:text-xl ">
+                wpp: +39 331 449 5391
+              </p>
             </div>
             <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
               <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
